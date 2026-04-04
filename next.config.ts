e85ec34df824770fb4a5c75b5bc95606ec550eaa
Jean-Ludovic <144408053+Jean-Ludovic@users.noreply.github.com>
2026-04-03 20:38:13 -0400

    preapring_for_prod

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,22 @@
-export default {
+import type { NextConfig } from 'next';
+
+const config: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        search: ''
+        hostname: 'avatars.githubusercontent.com'
       },
       {
         protocol: 'https',
-        hostname: '*.public.blob.vercel-storage.com',
-        search: ''
+        hostname: 'lh3.googleusercontent.com'
+      },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com'
       }
     ]
   }
 };
+
+export default config;
